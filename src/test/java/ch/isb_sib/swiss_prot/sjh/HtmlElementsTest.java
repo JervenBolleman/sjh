625d79f123173dc5bce09c106aac0c389a92c593
Jerven Bolleman <jerven.bolleman@isb-sib.ch>
2016-02-09 10:47:37 +0100

    Add test cases for the id or class convience methods

--- src/test/java/ch/isb_sib/swiss_prot/sjh/HtmlElementsTest.java
+++ src/test/java/ch/isb_sib/swiss_prot/sjh/HtmlElementsTest.java
@@ -1,7 +1,7 @@
 package ch.isb_sib.swiss_prot.sjh;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -10,6 +10,8 @@ import java.lang.reflect.Method;
 
 import org.junit.Test;
 
+import ch.isb_sib.swiss_prot.sjh.attributes.global.Clazz;
+import ch.isb_sib.swiss_prot.sjh.attributes.global.Id;
 import ch.isb_sib.swiss_prot.sjh.elements.Element;
 import ch.isb_sib.swiss_prot.sjh.elements.HTML;
 
@@ -32,6 +34,56 @@ public class HtmlElementsTest
 					test(el, "<" + name.toLowerCase() + "/>");
 			}
 		}
+	}
+
+	@Test
+	public void id()
+	    throws IOException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
+	{
+		Method[] methods = Elements.class.getDeclaredMethods();
+		int count = 0;
+		for (Method method : methods)
+		{
+
+			if (method.getParameterCount() == 1)
+			{
+				String name = method.getReturnType().getSimpleName();
+				Class<?>[] pt = method.getParameterTypes();
+				if (pt[0].equals(Id.class))
+				{
+					Element el = (Element) method.invoke(null, new Id("test"));
+					if (!(el instanceof HTML))
+						test(el, "<" + name.toLowerCase() + " id=\"test\"/>");
+					count++;
+				}
+			}
+		}
+		assertTrue("Make sure we test ", count > 10);
+	}
+
+	@Test
+	public void clazz()
+	    throws IOException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
+	{
+		Method[] methods = Elements.class.getDeclaredMethods();
+		int count = 0;
+		for (Method method : methods)
+		{
+
+			if (method.getParameterCount() == 1)
+			{
+				String name = method.getReturnType().getSimpleName();
+				Class<?>[] pt = method.getParameterTypes();
+				if (pt[0].equals(Clazz.class))
+				{
+					Element el = (Element) method.invoke(null, new Clazz("test"));
+					if (!(el instanceof HTML))
+						test(el, "<" + name.toLowerCase() + " class=\"test\"/>");
+					count++;
+				}
+			}
+		}
+		assertTrue("Make sure we test ", count > 10);
 	}
 
 	private void test(Element el, String expectedRaw)
