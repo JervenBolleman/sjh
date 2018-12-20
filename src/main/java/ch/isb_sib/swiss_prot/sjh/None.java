f709d1578f158f6df1fb9f1445a3d9dfca2e0b78
Jerven Bolleman <jerven.bolleman@sib.swiss>
2018-12-20 14:57:02 +0100

    None element explained and code layout fixed with eclipse.

--- src/main/java/ch/isb_sib/swiss_prot/sjh/None.java
+++ src/main/java/ch/isb_sib/swiss_prot/sjh/None.java
@@ -6,21 +6,25 @@ import java.io.Writer;
 
 import ch.isb_sib.swiss_prot.sjh.elements.Element;
 
+/**
+ * This class is to be inserted in places where an element is expected but for
+ * some reason you don't actually want to give the element.
+ */
 public class None implements Element {
-    public static final None none = new None();
+	public static final None none = new None();
 
-    private None() {
+	private None() {
 
-    }
+	}
 
-    @Override
-    public void render(OutputStream stream) throws IOException {
+	@Override
+	public void render(OutputStream stream) throws IOException {
 
-    }
+	}
 
-    @Override
-    public void render(Writer write) throws IOException {
+	@Override
+	public void render(Writer write) throws IOException {
 
-    }
+	}
 
 }
--- src/main/java/ch/isb_sib/swiss_prot/sjh/elements/CommonElement.java
+++ src/main/java/ch/isb_sib/swiss_prot/sjh/elements/CommonElement.java
@@ -12,99 +12,99 @@ import ch.isb_sib.swiss_prot.sjh.attributes.Attribute;
 import ch.isb_sib.swiss_prot.sjh.attributes.global.GlobalAttribute;
 
 public abstract class CommonElement implements Element {
-    private final Stream<GlobalAttribute> attributes;
-    private final Stream<? extends Element> childeren;
+	private final Stream<GlobalAttribute> attributes;
+	private final Stream<? extends Element> childeren;
 
-    protected abstract byte[] getName();
+	protected abstract byte[] getName();
 
-    public void render(OutputStream stream) throws IOException {
-	stream.write('<');
-	stream.write(getName());
-	Iterator<Attribute> sa = getAttributes().iterator();
-	if (sa.hasNext())
-	    stream.write(' ');
-	while (sa.hasNext()) {
-	    sa.next().render(stream);
-	    if (sa.hasNext())
-		stream.write(' ');
-	}
-	if (childeren != null) {
-
-	    Iterator<? extends Element> iter = childeren.iterator();
-	    if (iter.hasNext()) {
-		stream.write('>');
-		while (iter.hasNext())
-		    iter.next().render(stream);
+	public void render(OutputStream stream) throws IOException {
 		stream.write('<');
-		stream.write('/');
 		stream.write(getName());
-		stream.write('>');
-	    } else {
-		stream.write('/');
-		stream.write('>');
-	    }
-	} else {
-	    stream.write('/');
-	    stream.write('>');
-	}
-    }
+		Iterator<Attribute> sa = getAttributes().iterator();
+		if (sa.hasNext())
+			stream.write(' ');
+		while (sa.hasNext()) {
+			sa.next().render(stream);
+			if (sa.hasNext())
+				stream.write(' ');
+		}
+		if (childeren != null) {
 
-    public void render(Writer writer) throws IOException {
-	writer.write('<');
-	writer.write(new String(getName(), StandardCharsets.UTF_8));
-	Iterator<Attribute> sa = getAttributes().iterator();
-	if (sa.hasNext())
-	    writer.write(' ');
-	while (sa.hasNext()) {
-	    sa.next().render(writer);
-	    if (sa.hasNext())
-		writer.write(' ');
+			Iterator<? extends Element> iter = childeren.iterator();
+			if (iter.hasNext()) {
+				stream.write('>');
+				while (iter.hasNext())
+					iter.next().render(stream);
+				stream.write('<');
+				stream.write('/');
+				stream.write(getName());
+				stream.write('>');
+			} else {
+				stream.write('/');
+				stream.write('>');
+			}
+		} else {
+			stream.write('/');
+			stream.write('>');
+		}
 	}
-	if (childeren != null) {
 
-	    Iterator<? extends Element> iter = childeren.iterator();
-	    if (iter.hasNext()) {
-		writer.write('>');
-		while (iter.hasNext())
-		    iter.next().render(writer);
+	public void render(Writer writer) throws IOException {
 		writer.write('<');
-		writer.write('/');
 		writer.write(new String(getName(), StandardCharsets.UTF_8));
-		writer.write('>');
-	    } else {
-		writer.write('/');
-		writer.write('>');
-	    }
-	} else {
-	    writer.write('/');
-	    writer.write('>');
+		Iterator<Attribute> sa = getAttributes().iterator();
+		if (sa.hasNext())
+			writer.write(' ');
+		while (sa.hasNext()) {
+			sa.next().render(writer);
+			if (sa.hasNext())
+				writer.write(' ');
+		}
+		if (childeren != null) {
+
+			Iterator<? extends Element> iter = childeren.iterator();
+			if (iter.hasNext()) {
+				writer.write('>');
+				while (iter.hasNext())
+					iter.next().render(writer);
+				writer.write('<');
+				writer.write('/');
+				writer.write(new String(getName(), StandardCharsets.UTF_8));
+				writer.write('>');
+			} else {
+				writer.write('/');
+				writer.write('>');
+			}
+		} else {
+			writer.write('/');
+			writer.write('>');
+		}
 	}
-    }
 
-    protected CommonElement(Stream<GlobalAttribute> ga, Stream<? extends Element> childeren) {
-	super();
-	this.attributes = ga;
-	this.childeren = childeren.filter(Objects::nonNull);
-    }
+	protected CommonElement(Stream<GlobalAttribute> ga, Stream<? extends Element> childeren) {
+		super();
+		this.attributes = ga;
+		this.childeren = childeren.filter(Objects::nonNull);
+	}
 
-    protected CommonElement() {
-	super();
-	this.attributes = null;
-	this.childeren = null;
-    }
+	protected CommonElement() {
+		super();
+		this.attributes = null;
+		this.childeren = null;
+	}
 
-    private Stream<GlobalAttribute> getGlobalAttributes() {
-	if (attributes != null)
-	    return attributes.filter(Objects::nonNull);
-	else
-	    return Stream.empty();
-    }
+	private Stream<GlobalAttribute> getGlobalAttributes() {
+		if (attributes != null)
+			return attributes.filter(Objects::nonNull);
+		else
+			return Stream.empty();
+	}
 
-    private final Stream<Attribute> getAttributes() {
-	return Stream.concat(getGlobalAttributes(), getElementSpecificAttributes()).filter(Objects::nonNull);
-    }
+	private final Stream<Attribute> getAttributes() {
+		return Stream.concat(getGlobalAttributes(), getElementSpecificAttributes()).filter(Objects::nonNull);
+	}
 
-    protected Stream<Attribute> getElementSpecificAttributes() {
-	return Stream.empty();
-    }
+	protected Stream<Attribute> getElementSpecificAttributes() {
+		return Stream.empty();
+	}
 }
